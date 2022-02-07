b396d31f0cae22bc311914727aa865d1662a5acb
Alan Guedes <alanlivio@gmail.com>
2022-02-07 15:25:34 +0000

    update voroni

--- exp_voroni.py
+++ exp_voroni.py
@@ -1,38 +1,41 @@
 # Trinity paches
 import math
 import numpy as np
-trinity_npatchs = 14
-trinity_points = np.empty((0, 3))
-for i in range(0, trinity_npatchs-1):
-    zi = (1 - 1.0/trinity_npatchs) * (1 - 2.0*i / (trinity_npatchs - 1))
+TRINITY_NPATCHS = 14
+TRINITY_POINTS = np.empty((0, 3))
+for i in range(0, TRINITY_NPATCHS-1):
+    zi = (1 - 1.0/TRINITY_NPATCHS) * (1 - 2.0*i / (TRINITY_NPATCHS - 1))
     di = math.sqrt(1 - math.pow(zi, 2))
     alphai = i * math.pi * (3 - math.sqrt(5))
     xi = di * math.cos(alphai)
     yi = di * math.sin(alphai)
     new_point = np.array([[xi, yi, zi]])
-    trinity_points = np.append(trinity_points, new_point, axis=0)
-# trinity_points = np.array([[0, 0, 1], [0, 0, -1], [1, 0, 0], [0, 1, 0], [0, -1, 0], [-1, 0, 0]])
+    TRINITY_POINTS = np.append(TRINITY_POINTS, new_point, axis=0)
+# TRINITY_POINTS = np.array([[0, 0, 1], [0, 0, -1], [1, 0, 0], [0, 1, 0], [0, -1, 0], [-1, 0, 0]])
+
+
+# vi for head_motion_predction
+SAMPLED_DATASET = None
 
 
 def vis_voroni_plotly(trajectory=None, save_html=False):
     import numpy as np
-    import pandas as pd
     import plotly
     import plotly.graph_objs as go
     from scipy.spatial import SphericalVoronoi, geometric_slerp
     import numpy as np
     radius = 1
     center = np.array([0, 0, 0])
-    sv = SphericalVoronoi(trinity_points, radius, center)
+    sv = SphericalVoronoi(TRINITY_POINTS, radius, center)
     sv.sort_vertices_of_regions()
 
     # -- plot the voronoi tessellation
-    plotly.offline.init_notebook_mode()
+    # plotly.offline.init_notebook_mode()
     data = []
 
-    # -- plot generator trinity_points
-    # gens = go.Scatter3d(x=trinity_points[:, 0], y=trinity_points[:, 1], z=trinity_points[:, 2],
-    #                     mode='markers', marker={'size': 1, 'opacity': 1.0, 'color': 'blue'}, name='Generator trinity_Points')
+    # -- plot generator TRINITY_POINTS
+    # gens = go.Scatter3d(x=TRINITY_POINTS[:, 0], y=TRINITY_POINTS[:, 1], z=TRINITY_POINTS[:, 2],
+    #                     mode='markers', marker={'size': 1, 'opacity': 1.0, 'color': 'blue'}, name='Generator TRINITY_POINTS')
     # data.append(gens)
 
     # -- plot vortonoi vertices
@@ -62,38 +65,27 @@ def vis_voroni_plotly(trajectory=None, save_html=False):
             for user in trajectory.keys():
                 for video in trajectory[user].keys():
                     trajc = go.Scatter3d(x=trajectory[user][video][:, 0], y=trajectory[user][video][:, 1],
-                                         z=trajectory[user][video][:, 2], mode='lines', 
-                                         line={'width': 1, 'color': 'blue'}, 
-                                         name='trajectory',showlegend=False)
+                                         z=trajectory[user][video][:,
+                                                                   2], mode='lines',
+                                         line={'width': 1, 'color': 'blue'},
+                                         name='trajectory', showlegend=False)
                     data.append(trajc)
 
-    # -- configure layout
-    layout = go.Layout(margin={'l': 0, 'r': 0, 'b': 0, 't': 0}, title={
-        'text': "Trinity Voroni with trajectory",
-        'y': 0.9,
-        'x': 0.5,
-        'xanchor': 'center',
-        'yanchor': 'top'})
-    plot_figure = go.Figure(data=data, layout=layout)
-
     # -- render plot
-
     if save_html:
         plotly.offline.plot(data, filename=f'{__file__}.html', auto_open=False)
     else:
-        plotly.offline.iplot(plot_figure)
-
-
+        layout = go.Layout(margin={'l': 0, 'r': 0, 'b': 0, 't': 0}, title={
+            'y': 0.9,
+            'x': 0.5,
+            'xanchor': 'center'})
+        figure = go.Figure(data=data, layout=layout, layout_width=800)
+        figure.show()
+    
 def vis_voroni_matplot(trajectory=None):
     import numpy as np
-    import pandas as pd
-    from head_motion_prediction.Utils import eulerian_to_cartesian, cartesian_to_eulerian, rotationBetweenVectors, interpolate_quaternions, degrees_to_radian, radian_to_degrees, compute_orthodromic_distance, store_dict_as_csv
-    from pyquaternion import Quaternion
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
     from scipy.spatial import SphericalVoronoi, geometric_slerp
-    from mpl_toolkits.mplot3d import proj3d
     fig = plt.figure()
     fig.set_size_inches(18.5, 10.5)
     u, v = np.mgrid[0:2 * np.pi:20j, 0:np.pi:10j]
@@ -105,7 +97,7 @@ def vis_voroni_matplot(trajectory=None):
     ax.set_zlabel('Z')
     radius = 1
     center = np.array([0, 0, 0])
-    sv = SphericalVoronoi(trinity_points, radius, center)
+    sv = SphericalVoronoi(TRINITY_POINTS, radius, center)
     sv.sort_vertices_of_regions()
     t_vals = np.linspace(0, 1, 2000)
     # plot the unit sphere for reference (optional)
@@ -114,9 +106,9 @@ def vis_voroni_matplot(trajectory=None):
     x = np.outer(np.cos(u), np.sin(v))
     y = np.outer(np.sin(u), np.sin(v))
     z = np.outer(np.ones(np.size(u)), np.cos(v))
-    # plot generator trinity_points
-    ax.scatter(trinity_points[:, 0], trinity_points[:,
-               1], trinity_points[:, 2], c='b')
+    # plot generator TRINITY_POINTS
+    ax.scatter(TRINITY_POINTS[:, 0], TRINITY_POINTS[:,
+               1], TRINITY_POINTS[:, 2], c='b')
     # plot Voronoi vertices
     ax.scatter(sv.vertices[:, 0], sv.vertices[:, 1], sv.vertices[:, 2],
                c='g')
@@ -135,9 +127,6 @@ def vis_voroni_matplot(trajectory=None):
         ax.plot(trajectory[:, 0], trajectory[:, 1],
                 trajectory[:, 2], label='parametric curve')
     plt.show()
-
-
-SAMPLED_DATASET = None
 
 
 def get_sample_dataset():
