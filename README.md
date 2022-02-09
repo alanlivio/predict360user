6757eb2df24b8d4178b394d8c356d1ecb8762678
Alan Guedes <alanlivio@gmail.com>
2022-02-09 18:40:10 +0000

    add plot_voroni_one_video_one_all_user

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Experiments on omnidirecional video with the following repos:
 ```bash
 git clone https://gitlab.com/miguelfromeror/head-motion-prediction head_motion_prediction 
 git clone https://github.com/manojMadarasingha/OpCASH
+git clone https://github.com/pkuguanyu/PanoProject
 git clone https://github.com/sarthak-chakraborty/PARIMA/
 git clone https://github.com/blackwiz4rd/TileRateSelector_360videos
 ```
--- exp_voroni.py
+++ exp_voroni.py
@@ -1,6 +1,11 @@
-# Trinity paches
 import math
 import numpy as np
+import plotly
+import plotly.graph_objs as go
+from scipy.spatial import SphericalVoronoi, geometric_slerp
+
+
+# Trinity paches
 TRINITY_NPATCHS = 14
 TRINITY_POINTS = np.empty((0, 3))
 for i in range(0, TRINITY_NPATCHS-1):
@@ -11,81 +16,38 @@ for i in range(0, TRINITY_NPATCHS-1):
     yi = di * math.sin(alphai)
     new_point = np.array([[xi, yi, zi]])
     TRINITY_POINTS = np.append(TRINITY_POINTS, new_point, axis=0)
-# TRINITY_POINTS = np.array([[0, 0, 1], [0, 0, -1], [1, 0, 0], [0, 1, 0], [0, -1, 0], [-1, 0, 0]])
+
+# head_motion_predction sample dataset
+SAMPLE_DATASET = None
+ONE_USER = '0'
+ONE_VIDEO = '10_Cows'
 
 
-# vi for head_motion_predction
-SAMPLED_DATASET = None
+def get_sample_dataset():
+    import sys
+    import os
+    global SAMPLE_DATASET
+    sys.path.append('head_motion_prediction')
+    from head_motion_prediction.David_MMSys_18.Read_Dataset import load_sampled_dataset
+    project_path = "head_motion_prediction"
+    cwd = os.getcwd()
+    if SAMPLE_DATASET is None:
+        if os.path.basename(cwd) != project_path:
+            print(f"running get_sample_dataset on {project_path}")
+            os.chdir(project_path)
+            SAMPLE_DATASET = load_sampled_dataset()
+            os.chdir(cwd)
+    return SAMPLE_DATASET
 
 
-def vis_voroni_plotly(trajectory=None, save_html=False):
-    import numpy as np
-    import plotly
-    import plotly.graph_objs as go
-    from scipy.spatial import SphericalVoronoi, geometric_slerp
-    import numpy as np
-    radius = 1
-    center = np.array([0, 0, 0])
-    sv = SphericalVoronoi(TRINITY_POINTS, radius, center)
-    sv.sort_vertices_of_regions()
+def get_sample_one_user_one_video():
+    return get_sample_dataset()[ONE_USER][ONE_VIDEO][:, 1:]
 
-    # -- plot the voronoi tessellation
-    # plotly.offline.init_notebook_mode()
-    data = []
 
-    # -- plot generator TRINITY_POINTS
-    # gens = go.Scatter3d(x=TRINITY_POINTS[:, 0], y=TRINITY_POINTS[:, 1], z=TRINITY_POINTS[:, 2],
-    #                     mode='markers', marker={'size': 1, 'opacity': 1.0, 'color': 'blue'}, name='Generator TRINITY_POINTS')
-    # data.append(gens)
+# plot functions
 
-    # -- plot vortonoi vertices
-    vrts = go.Scatter3d(x=sv.vertices[:, 0], y=sv.vertices[:, 1], z=sv.vertices[:, 2],
-                        mode='markers', marker={'size': 1, 'opacity': 1.0, 'color': 'green'}, name='Voronoi Vertices')
-    data.append(vrts)
-
-    for region in sv.regions:
-        n = len(region)
-        # -- get color for region
-        rgb = np.random.rand(3,)
-        t = np.linspace(0, 1, 100)
-        for i in range(n):
-            start = sv.vertices[region][i]
-            end = sv.vertices[region][(i + 1) % n]
-            result = np.array(geometric_slerp(start, end, t))
-            edge = go.Scatter3d(x=result[..., 0], y=result[..., 1], z=result[..., 2], mode='lines', line={
-                                'width': 1, 'color': [rgb]}, name='region edge', showlegend=False)
-            data.append(edge)
-
-    if trajectory is not None:
-        if isinstance(trajectory, np.ndarray):
-            trajc = go.Scatter3d(x=trajectory[:, 0], y=trajectory[:, 1], z=trajectory[:, 2],
-                                 mode='lines', line={'width': 1, 'color': 'blue'}, name='trajectory', showlegend=False)
-            data.append(trajc)
-        else:
-            for user in trajectory.keys():
-                for video in trajectory[user].keys():
-                    trajc = go.Scatter3d(x=trajectory[user][video][:, 0], y=trajectory[user][video][:, 1],
-                                         z=trajectory[user][video][:,
-                                                                   2], mode='lines',
-                                         line={'width': 1, 'color': 'blue'},
-                                         name='trajectory', showlegend=False)
-                    data.append(trajc)
-
-    # -- render plot
-    if save_html:
-        plotly.offline.plot(data, filename=f'{__file__}.html', auto_open=False)
-    else:
-        layout = go.Layout(margin={'l': 0, 'r': 0, 'b': 0, 't': 0}, title={
-            'y': 0.9,
-            'x': 0.5,
-            'xanchor': 'center'})
-        figure = go.Figure(data=data, layout=layout, layout_width=800)
-        figure.show()
-    
-def vis_voroni_matplot(trajectory=None):
-    import numpy as np
+def plot_voroni_one_user_one_video_with_matplot():
     import matplotlib.pyplot as plt
-    from scipy.spatial import SphericalVoronoi, geometric_slerp
     fig = plt.figure()
     fig.set_size_inches(18.5, 10.5)
     u, v = np.mgrid[0:2 * np.pi:20j, 0:np.pi:10j]
@@ -123,30 +85,72 @@ def vis_voroni_matplot(trajectory=None):
                     result[..., 1],
                     result[..., 2],
                     c='k')
-    if trajectory is not None:
-        ax.plot(trajectory[:, 0], trajectory[:, 1],
-                trajectory[:, 2], label='parametric curve')
+
+    trajectory = get_sample_dataset()[ONE_USER][ONE_VIDEO][:, 1:]
+    ax.plot(trajectory[:, 0], trajectory[:, 1],
+            trajectory[:, 2], label='parametric curve')
     plt.show()
 
 
-def get_sample_dataset():
-    import sys
-    import os
-    global SAMPLED_DATASET
-    sys.path.append('head_motion_prediction')
-    from head_motion_prediction.David_MMSys_18.Read_Dataset import load_sampled_dataset
-    project_path = "head_motion_prediction"
-    cwd = os.getcwd()
-    if SAMPLED_DATASET is None:
-        if os.path.basename(cwd) != project_path:
-            print(f"running load_sampled_dataset on {project_path}")
-            os.chdir(project_path)
-            SAMPLED_DATASET = load_sampled_dataset()
-            os.chdir(cwd)
-    return SAMPLED_DATASET
+def plotly_data_voroni():
+    radius = 1
+    center = np.array([0, 0, 0])
+    sv = SphericalVoronoi(TRINITY_POINTS, radius, center)
+    sv.sort_vertices_of_regions()
+    data = []
+
+    # -- add generator points
+    # gens = go.Scatter3d(x=TRINITY_POINTS[:, 0], y=TRINITY_POINTS[:, 1], z=TRINITY_POINTS[:, 2], mode='markers', marker={'size': 1, 'opacity': 1.0, 'color': 'blue'}, name='Generator TRINITY_POINTS')
+    # data.append(gens)
+
+    # -- add vortonoi vertices
+    vrts = go.Scatter3d(x=sv.vertices[:, 0], y=sv.vertices[:, 1], z=sv.vertices[:, 2],
+                        mode='markers', marker={'size': 1, 'opacity': 1.0, 'color': 'green'}, name='Voronoi Vertices')
+    data.append(vrts)
+
+    # -- add vortonoi edges
+    for region in sv.regions:
+        n = len(region)
+        rgb = np.random.rand(3,)
+        t = np.linspace(0, 1, 100)
+        for i in range(n):
+            start = sv.vertices[region][i]
+            end = sv.vertices[region][(i + 1) % n]
+            result = np.array(geometric_slerp(start, end, t))
+            edge = go.Scatter3d(x=result[..., 0], y=result[..., 1], z=result[..., 2], mode='lines', line={
+                                'width': 1, 'color': [rgb]}, name='region edge', showlegend=False)
+            data.append(edge)
+    return data
 
 
-def get_sample_user_trace():
-    user = '0'
-    video = '10_Cows'
-    return get_sample_dataset()[user][video][:, 1:]
+def plotly_data_append_user_traces(data, dataset, user, video):
+    trajc = go.Scatter3d(x=dataset[user][video][:, 1:][:, 0],
+                         y=dataset[user][video][:, 1:][:, 1],
+                         z=dataset[user][video][:, 1:][:, 2],
+                         mode='lines',
+                         line={'width': 1, 'color': 'blue'},
+                         name='trajectory', showlegend=False)
+    data.append(trajc)
+
+
+def plot_voroni_one_video_one_user(to_html=False):
+    data = plotly_data_voroni()
+    dataset = get_sample_dataset()
+    plotly_data_append_user_traces(data, dataset, ONE_USER, ONE_VIDEO)
+    if to_html:
+        plotly.offline.plot(data, filename=f'{__file__}.html', auto_open=False)
+    else:
+        layout = go.Layout(margin={'l': 0, 'r': 0, 'b': 0, 't': 0})
+        go.Figure(data=data, layout=layout, layout_width=800).show()
+
+
+def plot_voroni_one_video_one_all_user(to_html=False):
+    data = plotly_data_voroni()
+    dataset = get_sample_dataset()
+    for user in dataset.keys():
+        plotly_data_append_user_traces(data, dataset, user, ONE_VIDEO)
+    if to_html:
+        plotly.offline.plot(data, filename=f'{__file__}.html', auto_open=False)
+    else:
+        layout = go.Layout(margin={'l': 0, 'r': 0, 'b': 0, 't': 0})
+        go.Figure(data=data, layout=layout, layout_width=800).show()
