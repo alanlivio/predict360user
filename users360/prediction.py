364882c20b5b2be36854dd72aae9eaeec2a38c33
Alan Guedes <alanlivio@gmail.com>
2022-11-23 00:55:12 +0000

    update 2-explore-prediction

--- users360/prediction.py
+++ users360/prediction.py
@@ -1,8 +1,4 @@
-from os.path import exists
-
-import numpy as np
 import pandas as pd
-import plotly.express as px
 from sklearn.model_selection import train_test_split
 
 from .data import *
@@ -18,13 +14,11 @@ def get_train_test_split(train_entropy, test_entropy, perc_test) -> tuple[pd.Dat
         if train_entropy == 'all':
             X_train, _ = train_test_split(df, test_size=perc_test, random_state=1)
         else:
-            X_train, _ = train_test_split(df[df['traject_entropy_class'] == train_entropy],
-                                          test_size=perc_test, random_state=1)
+            X_train, _ = train_test_split(df[df['traject_entropy_class'] == train_entropy], test_size=perc_test, random_state=1)
         if test_entropy == 'all':
             _, X_test = train_test_split(df, test_size=perc_test, random_state=1)
         else:
-            _, X_test = train_test_split(df[df['traject_entropy_class'] == test_entropy],
-                                         test_size=perc_test, random_state=1)
+            _, X_test = train_test_split(df[df['traject_entropy_class'] == test_entropy], test_size=perc_test, random_state=1)
     return X_train, X_test
 
 
