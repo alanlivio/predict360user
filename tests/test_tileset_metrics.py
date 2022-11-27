2167844cf26aab205a84f72ba17f649057b00c31
Alan Guedes <a.guedes@ucl.ac.uk>
2022-11-27 19:20:10 +0000

    rename tileset_metrics related files

--- tests/test_tileset_metrics.py
+++ tests/test_tileset_metrics.py
@@ -7,8 +7,8 @@ from users360.utils.tileset import TILESET_VARIATIONS_FOR_TEST
 
 class Test(unittest.TestCase):
 
-  def test_tilset_poles(self) -> None:
+  def test_tilset_metrics_poles(self) -> None:
     calc_trajects_poles_prc(testing=True)
 
-  def test_tileset_reqs_metrics(self) -> None:
+  def test_tileset_metrics_reqs(self) -> None:
     calc_tileset_reqs_metrics(TILESET_VARIATIONS_FOR_TEST, testing=True)
