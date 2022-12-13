ab149f0ff3fb6d2bd9d239ece700aca516eefc66
Alan Guedes <a.guedes@ucl.ac.uk>
2022-12-13 19:33:32 +0000

    refact get_train_test_split

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -18,120 +18,49 @@ class Test(unittest.TestCase):
 
   def test_prediction_train_test_split(self) -> None:
     # x_train all
-    x_train, x_test = get_train_test_split(self.df_trajects, 'all', 'all', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'all', 'hight', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'all', 'medium', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'all', 'low', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low']
+    x_train, x_test = get_train_test_split(self.df_trajects, 'all', 0.2)
+    unique_s = set(x_train['traject_entropy_class'].unique())
+    assert unique_s == set(['low', 'medium', 'hight'])
+    unique_s = set(x_test['traject_entropy_class'].unique())
+    assert unique_s == set(['low', 'medium', 'hight'])
 
     # x_train low
-    x_train, x_test = get_train_test_split(self.df_trajects, 'low', 'all', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'low', 'low', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'low', 'medium', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'low', 'hight', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
+    x_train, x_test = get_train_test_split(self.df_trajects, 'low', 0.2)
+    unique_s = set(x_train['traject_entropy_class'].unique())
+    assert unique_s == set(['low'])
+    unique_s = set(x_test['traject_entropy_class'].unique())
+    assert unique_s == set(['low'])
 
     # x_train medium
-    x_train, x_test = get_train_test_split(self.df_trajects, 'medium', 'all', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'medium', 'low', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'medium', 'medium', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'medium', 'hight', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
+    x_train, x_test = get_train_test_split(self.df_trajects, 'medium', 0.2)
+    unique_s = set(x_train['traject_entropy_class'].unique())
+    assert unique_s == set(['medium'])
+    unique_s = set(x_test['traject_entropy_class'].unique())
+    assert unique_s == set(['medium'])
 
     # x_train hight
-    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 'all', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low', 'medium', 'hight']
+    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 0.2)
+    unique_s = set(x_train['traject_entropy_class'].unique())
+    assert unique_s == set(['hight'])
+    unique_s = set(x_test['traject_entropy_class'].unique())
+    assert unique_s == set(['hight'])
 
-    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 'low', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['low']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 'medium', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == ['medium']
-
-    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 'hight', 0.2)
-    unique_l = x_train['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
-    unique_l = x_test['traject_entropy_class'].unique()
-    assert unique_l == 'hight'
 
   def test_prediction_train_test_split_user_entropy(self) -> None:
-    x_train, x_test = get_train_test_split(self.df_trajects, 'low_users', 'low_users', 0.2)
-    unique_l = x_train['user_entropy_class'].unique()
-    assert unique_l == 'low'
-    unique_l = x_test['user_entropy_class'].unique()
-    assert unique_l == 'low'
+    x_train, x_test = get_train_test_split(self.df_trajects, 'low_users', 0.2)
+    unique_s = set(x_train['user_entropy_class'].unique())
+    assert unique_s == set(['low'])
+    unique_s = set(x_test['user_entropy_class'].unique())
+    assert unique_s == set(['low'])
 
-    x_train, x_test = get_train_test_split(self.df_trajects, 'medium_users', 'medium_users', 0.2)
-    unique_l = x_train['user_entropy_class'].unique()
-    assert unique_l == 'medium'
-    unique_l = x_test['user_entropy_class'].unique()
-    assert unique_l == 'medium'
+    x_train, x_test = get_train_test_split(self.df_trajects, 'medium_users', 0.2)
+    unique_s = set(x_train['user_entropy_class'].unique())
+    assert unique_s == set(['medium'])
+    unique_s = set(x_test['user_entropy_class'].unique())
+    assert unique_s == set(['medium'])
 
-    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 'hight', 0.2)
-    unique_l = x_train['user_entropy_class'].unique()
-    assert unique_l == 'hight'
-    unique_l = x_test['user_entropy_class'].unique()
-    assert unique_l == 'hight'
+    x_train, x_test = get_train_test_split(self.df_trajects, 'hight', 0.2)
+    unique_s = set(x_train['user_entropy_class'].unique())
+    assert unique_s == set(['hight'])
+    unique_s = set(x_test['user_entropy_class'].unique())
+    assert unique_s == set(['hight'])
--- users360/prediction.py
+++ users360/prediction.py
@@ -8,8 +8,7 @@ from sklearn.model_selection import train_test_split
 from .entropy import show_trajects_entropy, show_trajects_entropy_users
 
 
-def get_train_test_split(df_trajects: pd.DataFrame, train_entropy: str,
-                         test_entropy: str,
+def get_train_test_split(df_trajects: pd.DataFrame, entropy: str,
                          perc_test: float) -> tuple[pd.DataFrame, pd.DataFrame]:
   args = {'test_size': perc_test, 'random_state': 1}
 
@@ -17,32 +16,20 @@ def get_train_test_split(df_trajects: pd.DataFrame, train_entropy: str,
   x_train, x_test = train_test_split(df_trajects, **args)
 
   # train
-  if train_entropy.endswith('_users') and train_entropy != 'all':
-    x_train, _ = train_test_split(
+  if entropy.endswith('_users') and entropy != 'all':
+    x_train, x_test = train_test_split(
         df_trajects[df_trajects['user_entropy_class'] ==
-                    train_entropy.removesuffix('_users')], **args)
-  elif train_entropy != 'all':
-    x_train, _ = train_test_split(
-        df_trajects[df_trajects['traject_entropy_class'] == train_entropy],
-        **args)
-
-  # test
-  if test_entropy.endswith('_users') and test_entropy != 'all':
-    _, test_entropy = train_test_split(
-        df_trajects[df_trajects['user_entropy_class'] ==
-                    test_entropy.removesuffix('_users')], **args)
-  elif test_entropy != 'all':
-    _, x_test = train_test_split(
-        df_trajects[df_trajects['traject_entropy_class'] == test_entropy],
+                    entropy.removesuffix('_users')], **args)
+  elif entropy != 'all':
+    x_train, x_test = train_test_split(
+        df_trajects[df_trajects['traject_entropy_class'] == entropy],
         **args)
 
   return x_train, x_test
 
 
-def show_train_test_split(df_trajects: pd.DataFrame, train_entropy: str,
-                          test_entropy: str, perc_test: float) -> None:
-  x_train, x_test = get_train_test_split(df_trajects, train_entropy,
-                                         test_entropy, perc_test)
+def show_train_test_split(df_trajects: pd.DataFrame, entropy: str, perc_test: float) -> None:
+  x_train, x_test = get_train_test_split(df_trajects, entropy, perc_test)
   x_train['partition'] = 'train'
   x_test['partition'] = 'test'
   df_trajects = pd.concat([x_train, x_test])
