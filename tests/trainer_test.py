4353c76588fdfb92efffba362b324d6f1246f4d7
Alan Guedes <alanlivio@gmail.com>
2023-02-15 11:42:53 +0000

    refact test_train_test_split_entropy

--- tests/trainer_test.py
+++ tests/trainer_test.py
@@ -25,23 +25,41 @@ class Test(unittest.TestCase):
       self.assertEqual(trn.model_dir, join(config.DATADIR, model_fullname))
       self.assertEqual(trn.using_auto, train_entropy.startswith('auto'))
 
-  def test_split(self) -> None:
-    # actS
-    ds = Dataset()
-    x_train, x_test = train_test_split_entropy(ds.df,'actS','all',0.2)
-    self.assertSequenceEqual(count_entropy(x_train, 'actS'), (10301, 6125, 3133, 1043))
-    self.assertSequenceEqual(count_entropy(x_test, 'actS'), (2576, 1510, 814, 252))
-    x_train, x_test = train_test_split_entropy(ds.df, 'actS','low',0.2)
-    self.assertSequenceEqual(count_entropy(x_train, 'actS'), (6108, 6108, 0, 0))
-    self.assertSequenceEqual(count_entropy(x_test, 'actS'), (1527, 1527, 0, 0))
-    x_train, x_test = train_test_split_entropy(ds.df, 'actS','medium',0.2)
-    self.assertSequenceEqual(count_entropy(x_train, 'actS'), (3157, 0, 3157, 0))
-    self.assertSequenceEqual(count_entropy(x_test, 'actS'), (790, 0, 790, 0))
-    x_train, x_test = train_test_split_entropy(ds.df, 'actS','hight',0.2)
-    self.assertSequenceEqual(count_entropy(x_train, 'actS'), (1036, 0, 0, 1036))
-    self.assertSequenceEqual(count_entropy(x_test, 'actS'), (259, 0, 0, 259))
-    x_train, x_test = train_test_split_entropy(ds.df, 'actS','nohight',0.2)
-    self.assertSequenceEqual(count_entropy(x_train, 'actS'), (9265, 6089, 3176, 0))
-    self.assertSequenceEqual(count_entropy(x_test, 'actS'), (2317, 1546, 771, 0))
-    # hmpS
-    # TODO: add hmpS cases
+  def test_train_test_split_entropy(self) -> None:
+    self.ds = Dataset()
+    self.assertTrue(self.ds.df.size)
+
+    if not 'actS_c' in self.ds.df.columns:
+      calc_actual_entropy(self.ds.df)
+
+    for entropy_type in ['actS', 'hmpS']:
+      # all
+      x_train, x_test = train_test_split_entropy(self.ds.df, entropy_type, 'all', 0.2)
+      classes = set(x_train[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['low', 'medium', 'hight']))
+      classes = set(x_test[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['low', 'medium', 'hight']))
+      # low
+      x_train, x_test = train_test_split_entropy(self.ds.df, entropy_type, 'low', 0.2)
+      classes = set(x_train[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['low']))
+      classes = set(x_test[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['low']))
+      # medium
+      x_train, x_test = train_test_split_entropy(self.ds.df, entropy_type, 'medium', 0.2)
+      classes = set(x_train[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['medium']))
+      classes = set(x_test[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['medium']))
+      # nohight
+      x_train, x_test = train_test_split_entropy(self.ds.df, entropy_type, 'nohight', 0.2)
+      classes = set(x_train[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['low','medium']))
+      classes = set(x_test[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['low','medium']))
+      # hight
+      x_train, x_test = train_test_split_entropy(self.ds.df, entropy_type, 'hight', 0.2)
+      classes = set(x_train[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['hight']))
+      classes = set(x_test[entropy_type+'_c'].unique())
+      self.assertSequenceEqual(classes, set(['hight']))
