146e22479db23b7b09cbf77d601163fba6694bea
Alan Guedes <a.guedes@ucl.ac.uk>
2023-10-27 16:04:25 +0100

    simplify __ini__

--- predict360user/__init__.py
+++ predict360user/__init__.py
@@ -1,5 +1,9 @@
-from predict360user.ingest import *
-from predict360user.exploration import *
-from predict360user.train import *
-from predict360user.compare import *
-from predict360user.utils.plot360 import *
+from predict360user.ingest import Dataset
+from predict360user.exploration import (
+    show_trajects_representative,
+    show_entropy_histogram,
+    show_entropy_histogram_per_partition,
+)
+from predict360user.train import Trainer, TrainerCfg
+from predict360user.compare import compare_train_results, compare_eval_results
+from predict360user.utils.plot360 import Plot360
