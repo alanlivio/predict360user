2e6ff15250d4d2bc08efdd23463ade3fefe1c9be
Alan Guedes <a.guedes@ucl.ac.uk>
2023-10-29 12:01:38 +0000

    add models/baselines.py

--- predict360user/base_model.py
+++ predict360user/base_model.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 import numpy as np
 import tensorflow as tf
-from .utils.utils import *
+from predict360user.utils.utils import *
 
 
 class BaseModel:
@@ -13,49 +13,6 @@ class BaseModel:
 
     def predict_for_sample(self, traces: np.array, x_i: int) -> np.array:
         pass
-
-
-class NoMotion(BaseModel):
-    def __init__(self, h_window) -> None:
-        super().__init__()
-        self.h_window = h_window
-
-    def generate_batch(
-        self, traces_l: list[np.array], x_i_l: list
-    ) -> Tuple[list, list]:
-        raise NotImplementedError
-
-    def predict_for_sample(self, traces: np.array, x_i) -> np.array:
-        model_pred = np.repeat(traces[x_i : x_i + 1], self.h_window, axis=0)
-        return model_pred
-
-
-class Interpolation(BaseModel):
-    def __init__(self, h_window) -> None:
-        super().__init__()
-        self.h_window = h_window
-
-    def generate_batch(
-        self, traces_l: list[np.array], x_i_l: list
-    ) -> Tuple[list, list]:
-        raise NotImplementedError
-
-    def predict_for_sample(self, traces: np.array, x_i) -> np.array:
-        rotation = rotationBetweenVectors(traces[x_i - 2], traces[x_i - 1])
-        prediction = [rotation.rotate(traces[x_i])]
-        for _ in range(self.h_window - 1):
-            prediction.append(rotation.rotate(prediction[-1]))
-        return prediction
-
-
-class Regression(BaseModel):
-    def generate_batch(
-        self, traces_l: list[np.array], x_i_l: list
-    ) -> Tuple[list, list]:
-        raise NotImplementedError
-
-    def predict_for_sample(self, traces: np.array, x_i) -> np.array:
-        raise NotImplementedError
 
 
 def metric_orth_dist_cartesian(position_a, position_b) -> float:
