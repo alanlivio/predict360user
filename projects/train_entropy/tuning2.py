8533820f011b0385206b2cb2d3cad2db2369ece7
Alan Guedes <a.guedes@ucl.ac.uk>
2023-11-28 18:43:27 +0000

    add tuning2 and tuning3 projects

--- projects/train_entropy/tuning2.py
+++ projects/train_entropy/tuning2.py
@@ -1,19 +1,29 @@
 from omegaconf import OmegaConf
 import logging
-from predict360user.model_config import ModelConf, build_run_name
-from predict360user.train import build_model, fit_keras, evaluate
+from dataclasses import dataclass
+import math
+import pandas as pd
 import wandb
+
+from predict360user.train import build_model, fit_keras, evaluate
 from predict360user.ingest import count_entropy, load_df_wins, split
+from predict360user.model_config import ModelConf, build_run_name, ENTROPY_NAMES_UNIQUE
 
 log = logging.getLogger()
 
 
-def run(cfg: ModelConf) -> None:
+@dataclass
+class RunConf(ModelConf):
+    train_entropy: str = ""
+
+
+def run(cfg: RunConf) -> None:
     build_run_name(cfg)
-    # cfg.run_name += f",100epochs"
+    assert cfg.train_entropy in ENTROPY_NAMES_UNIQUE
+    cfg.run_name += f",tuni2={cfg.train_entropy}"
     log.info(f"run conf is: \n--\n" + OmegaConf.to_yaml(cfg) + "--")
 
-    # load dataset
+    # -- load dataset --
     df_wins = load_df_wins(
         dataset_name=cfg.dataset_name,
         init_window=cfg.init_window,
@@ -29,6 +39,7 @@ def run(cfg: ModelConf) -> None:
         project="predict360user",
         config={
             "model_name": cfg.model_name,
+            "train_entropy": cfg.train_entropy,
             "batch_size": cfg.batch_size,
             "lr": cfg.lr,
             "train_n_low": n_low,
@@ -38,9 +49,30 @@ def run(cfg: ModelConf) -> None:
         name=cfg.run_name,
     )
 
-    # fit model
+    # -- fit --
     model = build_model(cfg)
-    fit_keras(cfg, model, df_wins)
+
+    # split for tuning
+    train_wins = df_wins[df_wins["partition"] == "train"]
+    val_wins = df_wins[df_wins["partition"] == "val"]
+    tuning_prc = 0.33
+    train_wins_tuning = train_wins[train_wins["actS_c"] == cfg.train_entropy].sample(
+        frac=tuning_prc, random_state=1
+    )
+    val_wins_tuning = val_wins[val_wins["actS_c"] == cfg.train_entropy].sample(
+        frac=tuning_prc, random_state=1
+    )
+    wins_pretuning = df_wins.drop(train_wins_tuning.index).drop(val_wins_tuning.index)
+    wins_tuning = pd.concat([train_wins_tuning, val_wins_tuning])
+
+    # fit 1
+    fit_keras(cfg, model, wins_pretuning)
+    del model
+    # fit 2
+    cfg.epochs = math.ceil(cfg.epochs + cfg.epochs * tuning_prc)
+    cfg.lr = 0.0001
+    model = build_model(cfg)
+    fit_keras(cfg, model, wins_tuning)
 
     # evaluate and log to wandb
     err_per_class_dict = evaluate(cfg, model, df_wins)
@@ -55,5 +87,5 @@ def run(cfg: ModelConf) -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-    cfg = OmegaConf.merge(OmegaConf.structured(ModelConf), OmegaConf.from_cli())
+    cfg = OmegaConf.merge(OmegaConf.structured(RunConf), OmegaConf.from_cli())
     run(cfg)
