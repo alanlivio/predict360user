783bfaa68c6b7043a36f2e6a64be56fc0615537d
Alan Guedes <a.guedes@ucl.ac.uk>
2023-11-30 18:01:05 +0000

    move code from train to ModelWrapper

--- predict360user/model_wrapper.py
+++ predict360user/model_wrapper.py
@@ -3,11 +3,47 @@ from typing import Generator, Tuple
 
 import numpy as np
 import pandas as pd
+import logging
+import os
+from os.path import exists, join
+from tensorflow import keras
+
+import numpy as np
+import pandas as pd
+import wandb
+import absl
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from tqdm.auto import tqdm
+from wandb.keras import WandbMetricsLogger
+
+from predict360user.utils.math360 import orth_dist_cartesian
+
+log = logging.getLogger()
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+tqdm.pandas()
+
+MODEL_NAMES = [
+    "pos_only",
+    "pos_only_3d",
+    "no_motion",
+    "interpolation",
+    "TRACK",
+    "CVPR18",
+    "MM18",
+    "most_salient_point",
+]
+MODELS_NAMES_NO_TRAIN = ["no_motion", "interpolation"]
 
 EVAL_RES_CSV = "eval_results.csv"
 TRAIN_RES_CSV = "train_results.csv"
 DEFAULT_SAVEDIR = "saved"
 ENTROPY_NAMES_UNIQUE = ["low", "medium", "high"]
+
+
+# disable TF logging
+absl.logging.set_verbosity(absl.logging.ERROR)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
 @dataclass
@@ -45,16 +81,129 @@ class ModelWrapper:
     def predict_for_sample(self, traces: np.array, x_i: int) -> np.array:
         pass
 
+    def fit(self, df_wins: pd.DataFrame) -> dict:
+        pass
 
-def batch_generator(model, df_wins: pd.DataFrame, batch_size: int) -> Generator:
-    assert batch_size
-    while True:
-        for start in range(0, len(df_wins), batch_size):
-            end = (
-                start + batch_size
-                if start + batch_size <= len(df_wins)
-                else len(df_wins)
+    def evaluate(self, df_wins: pd.DataFrame) -> dict:
+        log.info("evaluate ...")
+
+        # calculate predictions errors
+        test_wins = df_wins[df_wins["partition"] == "test"]
+        t_range = list(range(self.cfg.h_window))
+
+        def _calc_pred_err(row) -> list[float]:
+            # return np.random.rand(cfg.h_window)  # for debugging
+            traces = row["traces"]
+            x_i = row["trace_id"]
+            pred_true = row["h_window"]
+            # predict
+            pred = self.model.predict_for_sample(traces, x_i)
+            assert len(pred) == self.cfg.h_window
+            error_per_t = [orth_dist_cartesian(pred[t], pred_true[t]) for t in t_range]
+            return error_per_t
+
+        tqdm.pandas(
+            desc="evaluate",
+            ascii=True,
+            mininterval=60,  # one min
+        )
+        df_wins.loc[test_wins.index, t_range] = test_wins.progress_apply(
+            _calc_pred_err, axis=1, result_type="expand"
+        )
+        assert df_wins.loc[test_wins.index, t_range].all().all()
+
+        # calculate predications errors mean
+        classes = [
+            ("all", test_wins.index),
+            ("low", test_wins.index[test_wins["actS_c"] == "low"]),
+            ("nohigh", test_wins.index[test_wins["actS_c"] != "high"]),
+            ("medium", test_wins.index[test_wins["actS_c"] == "medium"]),
+            ("nolow", test_wins.index[test_wins["actS_c"] != "low"]),
+            ("high", test_wins.index[test_wins["actS_c"] == "high"]),
+        ]
+        ret = {tup[0]: {} for tup in classes}
+        for actS_c, idx in classes:
+            # 1) mean per class (as wandb summary): # err_all, err_low, err_nohigh, err_medium,
+            ret[actS_c]["mean"] = df_wins.loc[idx, t_range].values.mean()
+            # 2) mean err per t per class
+            class_err_per_t = df_wins.loc[idx, t_range].mean()
+            data = [[x, y] for (x, y) in zip(t_range, class_err_per_t)]
+            ret[actS_c]["mean_per_t"] = data
+        return ret
+
+    def batch_generator(self, df_wins: pd.DataFrame) -> Generator:
+        while True:
+            for start in range(0, len(df_wins), self.cfg.batch_size):
+                end = (
+                    start + self.cfg.batch_size
+                    if start + self.cfg.batch_size <= len(df_wins)
+                    else len(df_wins)
+                )
+                traces_l = df_wins[start:end]["traces"].values
+                x_i_l = df_wins[start:end]["trace_id"].values
+                yield self.generate_batch(traces_l, x_i_l)
+
+
+class KerasModelWrapper(ModelWrapper):
+    model: keras.Model
+
+    def fit(self, df_wins: pd.DataFrame) -> None:
+        log.info("train ...")
+        if self.cfg.model_name in MODELS_NAMES_NO_TRAIN:
+            return
+
+        model_dir = join(self.cfg.savedir, self.cfg.run_name)
+        train_csv_log_f = join(model_dir, TRAIN_RES_CSV)
+        model_path = join(model_dir, "weights.hdf5")
+
+        if not exists(model_dir):
+            os.makedirs(model_dir)
+        if exists(model_path):
+            self.model.load_weights(model_path)
+        log.info("model_path=" + model_path)
+
+        train_wins = df_wins[df_wins["partition"] == "train"]
+        val_wins = df_wins[df_wins["partition"] == "val"]
+        # calc initial_epoch
+        initial_epoch = 0
+        if exists(train_csv_log_f):
+            lines = pd.read_csv(train_csv_log_f)
+            lines.dropna(how="all", inplace=True)
+            done_epochs = int(lines.iloc[-1]["epoch"]) + 1
+            assert done_epochs <= self.cfg.epochs
+            initial_epoch = done_epochs
+            log.info(f"train_csv_log_f has {initial_epoch} epochs ")
+
+        # fit
+        if self.cfg.gpu_id:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(self.cfg.gpu_id)
+            log.info(f"set visible cpu to {self.cfg.gpu_id}")
+        if initial_epoch >= self.cfg.epochs:
+            log.info(
+                f"train_csv_log_f has {initial_epoch}>={self.cfg.epochs}. not training."
             )
-            traces_l = df_wins[start:end]["traces"].values
-            x_i_l = df_wins[start:end]["trace_id"].values
-            yield model.generate_batch(traces_l, x_i_l)
+        else:
+            steps_per_ep_train = np.ceil(len(train_wins) / self.cfg.batch_size)
+            steps_per_ep_validate = np.ceil(len(val_wins) / self.cfg.batch_size)
+            callbacks = [
+                CSVLogger(train_csv_log_f, append=True),
+                ModelCheckpoint(
+                    model_path,
+                    save_best_only=True,
+                    save_weights_only=True,
+                    mode="auto",
+                    period=1,
+                ),
+            ]
+            if wandb.run:
+                callbacks += [WandbMetricsLogger(initial_global_step=initial_epoch)]
+            self.model.fit_generator(
+                generator=self.batch_generator(train_wins),
+                validation_data=self.batch_generator(val_wins),
+                steps_per_epoch=steps_per_ep_train,
+                validation_steps=steps_per_ep_validate,
+                epochs=self.cfg.epochs,
+                initial_epoch=initial_epoch,
+                callbacks=callbacks,
+                verbose=2,
+            )
--- predict360user/train.py
+++ predict360user/train.py
@@ -3,16 +3,10 @@ import logging
 import os
 import sys
 from os.path import abspath, exists, isabs, isdir, join
-
-import absl.logging
 import IPython
-import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-from keras.callbacks import CSVLogger, ModelCheckpoint
-from tqdm.auto import tqdm
-from wandb.keras import WandbMetricsLogger
 
 from predict360user.model_wrapper import (
     DEFAULT_SAVEDIR,
@@ -20,157 +14,23 @@ from predict360user.model_wrapper import (
     TRAIN_RES_CSV,
     ModelWrapper,
     ModelConf,
-    batch_generator,
 )
 from predict360user.models import TRACK, Interpolation, NoMotion, PosOnly, PosOnly3D
-from predict360user.utils.math360 import orth_dist_cartesian
 
 log = logging.getLogger()
 
-MODEL_NAMES = [
-    "pos_only",
-    "pos_only_3d",
-    "no_motion",
-    "interpolation",
-    "TRACK",
-    "CVPR18",
-    "MM18",
-    "most_salient_point",
-]
-MODELS_NAMES_NO_TRAIN = ["no_motion", "interpolation"]
 
-
-log = logging.getLogger()
-
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-tqdm.pandas()
-
-# disable TF logging
-absl.logging.set_verbosity(absl.logging.ERROR)
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-
-
-def build_model(cfg) -> None:
+def build_model(cfg: ModelConf) -> ModelWrapper:
     if cfg.model_name == "pos_only":
         return PosOnly(cfg)
     elif cfg.model_name == "pos_only_3d":
         return PosOnly3D(cfg)
     elif cfg.model_name == "interpolation":
         return Interpolation(cfg)
-    elif cfg.model_name == "TRACK":
-        return TRACK(cfg)
     elif cfg.model_name == "no_motion":
         return NoMotion(cfg)
     else:
-        # TODO: initialize using config _target_
-        raise RuntimeError
-
-
-def fit_keras(cfg: ModelConf, model: ModelWrapper, df_wins: pd.DataFrame) -> None:
-    log.info("train ...")
-    if cfg.model_name in MODELS_NAMES_NO_TRAIN:
-        return
-
-    model_dir = join(cfg.savedir, cfg.run_name)
-    train_csv_log_f = join(model_dir, TRAIN_RES_CSV)
-    model_path = join(model_dir, "weights.hdf5")
-
-    if not exists(model_dir):
-        os.makedirs(model_dir)
-    if exists(model_path):
-        model.load_weights(model_path)
-    log.info("model_path=" + model_path)
-
-    train_wins = df_wins[df_wins["partition"] == "train"]
-    val_wins = df_wins[df_wins["partition"] == "val"]
-    # calc initial_epoch
-    initial_epoch = 0
-    if exists(train_csv_log_f):
-        lines = pd.read_csv(train_csv_log_f)
-        lines.dropna(how="all", inplace=True)
-        done_epochs = int(lines.iloc[-1]["epoch"]) + 1
-        assert done_epochs <= cfg.epochs
-        initial_epoch = done_epochs
-        log.info(f"train_csv_log_f has {initial_epoch} epochs ")
-
-    # fit
-    if cfg.gpu_id:
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg.gpu_id)
-        log.info(f"set visible cpu to {cfg.gpu_id}")
-    if initial_epoch >= cfg.epochs:
-        log.info(f"train_csv_log_f has {initial_epoch}>={cfg.epochs}. not training.")
-    else:
-        steps_per_ep_train = np.ceil(len(train_wins) / cfg.batch_size)
-        steps_per_ep_validate = np.ceil(len(val_wins) / cfg.batch_size)
-        callbacks = [
-            CSVLogger(train_csv_log_f, append=True),
-            ModelCheckpoint(
-                model_path,
-                save_best_only=True,
-                save_weights_only=True,
-                mode="auto",
-                period=1,
-            ),
-            WandbMetricsLogger(initial_global_step=initial_epoch),
-        ]
-        model.fit_generator(
-            generator=batch_generator(model, train_wins, cfg.batch_size),
-            validation_data=batch_generator(model, val_wins, cfg.batch_size),
-            steps_per_epoch=steps_per_ep_train,
-            validation_steps=steps_per_ep_validate,
-            epochs=cfg.epochs,
-            initial_epoch=initial_epoch,
-            callbacks=callbacks,
-            verbose=2,
-        )
-
-
-def evaluate(cfg: ModelConf, model: ModelWrapper, df_wins: pd.DataFrame) -> dict:
-    log.info("evaluate ...")
-
-    # calculate predictions errors
-    test_wins = df_wins[df_wins["partition"] == "test"]
-    t_range = list(range(cfg.h_window))
-
-    def _calc_pred_err(row) -> list[float]:
-        # return np.random.rand(cfg.h_window)  # for debugging
-        traces = row["traces"]
-        x_i = row["trace_id"]
-        pred_true = row["h_window"]
-        # predict
-        pred = model.predict_for_sample(traces, x_i)
-        assert len(pred) == cfg.h_window
-        error_per_t = [orth_dist_cartesian(pred[t], pred_true[t]) for t in t_range]
-        return error_per_t
-
-    tqdm.pandas(
-        desc="evaluate",
-        ascii=True,
-        mininterval=60,  # one min
-    )
-    df_wins.loc[test_wins.index, t_range] = test_wins.progress_apply(
-        _calc_pred_err, axis=1, result_type="expand"
-    )
-    assert df_wins.loc[test_wins.index, t_range].all().all()
-
-    # calculate predications errors mean
-    classes = [
-        ("all", test_wins.index),
-        ("low", test_wins.index[test_wins["actS_c"] == "low"]),
-        ("nohigh", test_wins.index[test_wins["actS_c"] != "high"]),
-        ("medium", test_wins.index[test_wins["actS_c"] == "medium"]),
-        ("nolow", test_wins.index[test_wins["actS_c"] != "low"]),
-        ("high", test_wins.index[test_wins["actS_c"] == "high"]),
-    ]
-    ret = {tup[0]: {} for tup in classes}
-    for actS_c, idx in classes:
-        # 1) mean per class (as wandb summary): # err_all, err_low, err_nohigh, err_medium,
-        ret[actS_c]["mean"] = df_wins.loc[idx, t_range].values.mean()
-        # 2) mean err per t per class
-        class_err_per_t = df_wins.loc[idx, t_range].mean()
-        data = [[x, y] for (x, y) in zip(t_range, class_err_per_t)]
-        ret[actS_c]["mean_per_t"] = data
-    return ret
+        raise NotImplementedError
 
 
 def show_or_save(output, savedir=DEFAULT_SAVEDIR, title="") -> None:
