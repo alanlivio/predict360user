f0be3bc7fdfc706a2a93b7971d06fc10fefd237b
Alan Guedes <a.guedes@ucl.ac.uk>
2024-01-26 17:26:30 +0000

    remove auto versions from filtred.py

--- experiments/train_entropy/filtred.py
+++ experiments/train_entropy/filtred.py
@@ -6,48 +6,7 @@ from dataclasses import dataclass
 import wandb
 import predict360user as p3u
 
-ENTROPY_NAMES = ["all", "low", "medium", "high", "nohigh", "nolow"]
-ENTROPY_NAMES_AUTO = ["auto", "auto_m_window", "auto_since_start"]
-
 log = logging.getLogger()
-
-
-def _set_predict_by_entropy(
-    model: p3u.ModelWrapper, cfg: p3u.Config, df_wins
-) -> p3u.ModelWrapper:
-    prefix = join(cfg.savedir, f"{cfg.model_name},{cfg.dataset_name},actS,")
-    model.threshold_medium, model.threshold_high = p3u.get_class_thresholds(
-        df_wins, "actS"
-    )
-    model.model_low = model.copy()
-    model.model_low.load_weights(join(prefix + "low", "weights.hdf5"))
-    model.model_medium = model.copy()
-    model.model_medium.load_weights(join(prefix + "medium", "weights.hdf5"))
-    model.model_high = model.copy()
-    model.model_high.load_weights(join(prefix + "high", "weights.hdf5"))
-
-    def _predict_by_entropy(self, traces: np.array, x_i: int) -> p3u.ModelWrapper:
-        if cfg.train_entropy == "auto":
-            window = traces
-        elif cfg.train_entropy == "auto_m_window":
-            window = traces[x_i - cfg.m_window : x_i]
-        elif cfg.train_entropy == "auto_since_start":
-            window = traces[0:x_i]
-        else:
-            raise RuntimeError()
-        a_ent = p3u.calc_actual_entropy(window)
-        actS_c = p3u.get_class_name(a_ent, self.threshold_medium, self.threshold_high)
-        if actS_c == "low":
-            return self.model_low.predict_for_sample(self, traces, x_i)
-        if actS_c == "medium":
-            return self.model_medium.predict_for_sample(self, traces, x_i)
-        if actS_c == "high":
-            return self.model_high.predict_for_sample(self, traces, x_i)
-        else:
-            raise RuntimeError()
-
-    model.predict_for_sample = _predict_by_entropy
-    return model
 
 
 @dataclass
@@ -61,7 +20,7 @@ def run(cfg: RunConfig) -> None:
     if cfg.minsize:
         run_name += f",mins={cfg.minsize!r}"
     log.info(f"run {run_name} config is: \n--\n" + OmegaConf.to_yaml(cfg) + "--")
-    assert cfg.train_entropy in ENTROPY_NAMES + ENTROPY_NAMES_AUTO
+    assert cfg.train_entropy in p3u.ENTROPY_NAMES_UNIQUE
     wandb.init(project="predict360user", name=run_name)
     wandb.run.log({"model": cfg.model_name, "batch": cfg.batch_size, "lr": cfg.lr})
 
@@ -85,8 +44,6 @@ def run(cfg: RunConfig) -> None:
 
     # fit model
     model = p3u.build_model(cfg)
-    if cfg.train_entropy.startswith("auto"):  # will not use model
-        _set_predict_by_entropy(model)
     model.fit(df_wins)
 
     # evaluate and log to wandb
