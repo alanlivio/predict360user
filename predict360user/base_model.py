67aa9f7e571383fab06ca6628323f9fc1ab93e3c
Alan Guedes <a.guedes@ucl.ac.uk>
2024-02-02 17:29:45 +0000

    fix fit to use cfg.name

--- predict360user/base_model.py
+++ predict360user/base_model.py
@@ -129,7 +129,7 @@ class KerasModel(BaseModel):
     def fit(self, df_wins: pd.DataFrame) -> None:
         log.info("train ...")
 
-        model_dir = join(DEFAULT_SAVEDIR, self.cfg.model)
+        model_dir = join(DEFAULT_SAVEDIR, self.cfg.name)
         train_csv_log_f = join(model_dir, TRAIN_RES_CSV)
         model_path = join(model_dir, "weights.hdf5")
 
